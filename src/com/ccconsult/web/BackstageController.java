32933748b8b3166bf0be8ea6b628629e6da7950a
dantianxuan <dantianxuan@qq.com>
2014-07-04 18:13:29 +0800

    阿斯达说的

---
+++ src/com/ccconsult/web/BackstageController.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.ccconsult.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author jingyu.dan
+ * 
+ */
+@Controller
+public class BackstageController extends BaseController {
+
+
+    @RequestMapping(value = "backstage/index.htm", method = RequestMethod.GET)
+    public ModelAndView toPage(HttpServletRequest request, ModelMap modelMap) {
+        ModelAndView view = new ModelAndView("backstage/index");
+        return view;
+    }
+
+}
