09764196d1a5f66010ce76dc7c43361b4bb6cf3e
dantianxuan <jingyudan@jingyudandeMacBook-Air.local>
2014-07-05 18:47:41 +0800

    最小代码、

---
+++ src/com/ccconsult/web/SearchController.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package com.ccconsult.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ccconsult.base.CcResult;
+import com.ccconsult.base.PageList;
+import com.ccconsult.dao.CompanyDAO;
+import com.ccconsult.dao.CounselorDAO;
+import com.ccconsult.pojo.Company;
+import com.ccconsult.pojo.Counselor;
+import com.ccconsult.util.StringUtil;
+import com.ccconsult.view.CompanyBriefVO;
+
+/**
+ * @author jingyu.dan
+ * 
+ */
+@Controller
+public class SearchController {
+
+    @Autowired
+    private CompanyDAO   companyDAO;
+    @Autowired
+    private CounselorDAO counselorDAO;
+
+    @RequestMapping(value = "/searchList.htm", method = RequestMethod.POST)
+    public ModelAndView handleRequest(HttpServletRequest request, String type, ModelMap modelMap) {
+        String keyword = request.getParameter("keyword");
+        CcResult result = new CcResult(true);
+        int pageSize = request.getParameter("pageSize") == null ? 20 : NumberUtils.toInt(request
+            .getParameter("pageSize"));
+        int pageNo = request.getParameter("pageNo") == null ? 1 : NumberUtils.toInt(request
+            .getParameter("pageNo"));
+        if (!StringUtil.equals("COUNSELOR", type)) {
+            type = "COMPANY";
+            PageList<CompanyBriefVO> companyBriefVOs = companyDAO.queryByName(pageNo, pageSize,
+                keyword);
+            result.setObject(companyBriefVOs);
+        } else {
+            PageList<Counselor> counselors = counselorDAO.queryByName(pageNo, pageSize, keyword);
+            result.setObject(counselors);
+        }
+        modelMap.put("keyword", keyword);
+        modelMap.put("type", type);
+        modelMap.put("result", result);
+        ModelAndView view = new ModelAndView("content/searchList");
+        return view;
+    }
+
+}
